72fa6c0f535c9a8e3b0fa9c878f438f9d33df1b6
Abdullah Elsayed <11673834+abdllahdev@users.noreply.github.com>
2024-01-04 01:00:17 +0200

    docs: update site description

--- src/consts.ts
+++ src/consts.ts
@@ -1,7 +1,7 @@
 export const SITE_NAME = "abdllah.dev";
 export const SITE_TITLE = "abdllah.dev";
 export const SITE_DESCRIPTION =
-  "Join me as I dive into the diverse realms of Computer Science – from Web Development to AI and Theoretical CS.";
+  "In this blog, I write about anything from Web Development to AI and Theoretical CS.";
 export const SITE_AUTHOR = "Abdullah Elsayed (@abdllahdev)";
 export const X_HANDLE = "abdllah_dev";
 export const GITHUB_HANDLE = "abdllahdev";
